078fef023bc65304aa48f5c4baa474dcd132dd1a
Dilakshan <dsivarub@my.centennialcollege.ca>
2023-10-01 03:21:31 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/experience', (req, res) => {
+  res.render('experience');
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects');
+});
+
+app.get('/services', (req, res) => {
+  res.render('services');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
